93ab9b0ec4d3982f69cd0efbfa31d236254abf27
Johan Ivarsson <86954289+Wickedviruz@users.noreply.github.com>
2023-12-02 18:16:40 +0100

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:plyls270@localhost:5432/todo'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/todo'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     Secret_Key = os.getenv('SECRET_KEY')
